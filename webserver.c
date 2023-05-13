02b648974172e966e9ee842a8e41d6f9efbc7709
EnzoDtoste <enzord2001@gmail.com>
2023-05-13 16:59:13 -0400

    finish order

--- webserver.c
+++ webserver.c
@@ -11,10 +11,17 @@
 #include <arpa/inet.h>
 #include <sys/sendfile.h>
 #include <math.h>
+#include <sys/signal.h>
 
 #define BUFFER_SIZE 102400
 #define TRUE 1
 #define FALSE 0
+
+enum {
+	NAME,
+    SIZE,
+	DATE
+};
 
 struct nodo {
     struct stat st;
@@ -121,6 +128,52 @@ void insertName(struct nodo* raiz, struct dirent* entry, struct stat st, int asc
     }   
 }
 
+void insertDate(struct nodo* raiz, struct dirent* entry, struct stat st, int asc)
+{   
+    double diff = difftime(raiz->st.st_mtime, st.st_mtime);
+    
+    if(asc == TRUE && diff < 0)
+    {
+        if(raiz->sig == NULL)
+        {
+            struct nodo *nuevo;
+            nuevo=malloc(sizeof(struct nodo));
+            nuevo->st = st;
+            nuevo->entry = entry;
+            nuevo->sig = NULL;
+            raiz->sig = nuevo;
+        }
+        else
+            insertDate(raiz->sig, entry, st, asc);
+    }
+    else if(asc == FALSE && diff > 0)
+    {
+        if(raiz->sig == NULL)
+        {
+            struct nodo *nuevo;
+            nuevo=malloc(sizeof(struct nodo));
+            nuevo->st = st;
+            nuevo->entry = entry;
+            nuevo->sig = NULL;
+            raiz->sig = nuevo;
+        }
+        else
+            insertDate(raiz->sig, entry, st, asc);
+    }
+    else
+    {
+        struct nodo *temp;
+        temp=malloc(sizeof(struct nodo));
+        temp->st = raiz->st;
+        temp->entry = raiz->entry;
+        temp->sig = raiz->sig;
+
+        raiz->entry = entry;
+        raiz->st = st;
+        raiz->sig = temp;
+    }   
+}
+
 void freeNodo(struct nodo* raiz)
 {
     struct nodo *reco = raiz;
@@ -134,6 +187,7 @@ void freeNodo(struct nodo* raiz)
 }
 
 char* init_path;
+int server_socket;
 
 int contarPorcentajes (char* str, int longitud) {
     int i = 0, j = 0;
@@ -207,7 +261,7 @@ void send_response(int client_socket, char* response, int response_length) {
     send(client_socket, response, response_length, 0);
 }
 
-void send_directory_listing(int client_socket, char* directory_path) {
+void send_directory_listing(int client_socket, char* directory_path, int fieldOrder, int Asc) {
     struct nodo* folders = NULL;
     struct nodo* files = NULL;
 
@@ -224,14 +278,28 @@ void send_directory_listing(int client_socket, char* directory_path) {
                 if(folders == NULL)
                     folders = create_list(entry, st);
                 else
-                    insertName(folders, entry, st, TRUE);
+                {
+                    if(fieldOrder == NAME)
+                        insertName(folders, entry, st, Asc);
+                    else if(fieldOrder == SIZE)
+                        insertName(folders, entry, st, TRUE);
+                    else if(fieldOrder == DATE)
+                        insertDate(folders, entry, st, Asc);
+                }
             }
             else
             {
                 if(files == NULL)
                     files = create_list(entry, st);
                 else
-                    insertName(files, entry, st, TRUE);
+                {
+                    if(fieldOrder == NAME)
+                        insertName(files, entry, st, Asc);
+                    else if(fieldOrder == SIZE)
+                        insertSize(files, entry, st, Asc);
+                    else if(fieldOrder == DATE)
+                        insertDate(files, entry, st, Asc);
+                }
             }
         }
         else
@@ -239,38 +307,85 @@ void send_directory_listing(int client_socket, char* directory_path) {
             if(files == NULL)
                 files = create_list(entry, st);
             else
-                insertName(files, entry, st, TRUE);
+            {
+                if(fieldOrder == NAME)
+                    insertName(files, entry, st, Asc);
+                else if(fieldOrder == SIZE)
+                    insertSize(files, entry, st, Asc);
+                else if(fieldOrder == DATE)
+                    insertDate(files, entry, st, Asc);
+            }
         }
     }
 
     char response[BUFFER_SIZE];
     int response_length = 0;
 
-    response_length += sprintf(response + response_length, "<html><head> <meta charset=\"UTF-8\"> <style> .bi-folder-fill-yellow{display:inline-block;width:1em;height:1em;vertical-align:-0.125em;background-image:url(\"data:image/svg+xml,%%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%%23f7d51d'%%3E%%3Cpath d='M14.5 6h-5.8l-1-2H4.5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM7 12H5v-2h2v2zm0-3H5V7h2v2z'/%%3E%%3C/svg%%3E\");background-repeat:no-repeat;background-size:1em} i { margin-right: 5px; } table{width:80%%;max-width:1000px;margin-left:auto;margin-right:auto;border-collapse:collapse}thead{background-color:#333;color:#fff}th{padding:12px}tbody tr:nth-child(even){background-color:#f2f2f2}td{padding:10px}td:first-child{text-align:left}td:last-child{text-align:center}td:nth-child(2){text-align:right;padding-right:20px} </style><title>Directorio %s</title></head><body><table><tr><th>Name</th><th>Size</th><th>Last Modification Date</th></tr>", directory_path);
+    if(Asc == FALSE)
+        response_length += sprintf(response + response_length, "<html><head> <meta charset=\"UTF-8\"> <style> .bi-folder-fill-yellow{display:inline-block;width:1em;height:1em;vertical-align:-0.125em;background-image:url(\"data:image/svg+xml,%%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%%23f7d51d'%%3E%%3Cpath d='M14.5 6h-5.8l-1-2H4.5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM7 12H5v-2h2v2zm0-3H5V7h2v2z'/%%3E%%3C/svg%%3E\");background-repeat:no-repeat;background-size:1em} i { margin-right: 5px; } table{width:80%%;max-width:1000px;margin-left:auto;margin-right:auto;border-collapse:collapse}thead{background-color:#333;color:#fff}th{padding:12px}tbody tr:nth-child(even){background-color:#f2f2f2}td{padding:10px}td:first-child{text-align:left}td:last-child{text-align:center}td:nth-child(2){text-align:right;padding-right:20px} #sinh { user-select: none; text-decoration: none; color: black; } </style><title>Directorio %s</title></head><body><table><tr><th><a id=\"sinh\" href=\"?order=nameAsc\">Name</a></th><th><a id=\"sinh\" href=\"?order=sizeAsc\">Size</a></th><th><a id=\"sinh\" href=\"?order=dateAsc\">Last Modification Date</a></th></tr>", directory_path);
 
-    struct nodo* folder = folders;
-    while(folder != NULL)
-    {
-        struct tm* t = localtime(&(folder->st).st_mtime);
-        char buffer [20];
-        strftime(buffer, 20, "%d-%m-%Y %H:%M", t); 
-        response_length += sprintf(response + response_length, "<tr><td><i class=\"bi bi-folder-fill-yellow\"></i><a href=\"%s/\">%s</a></td><td></td><td>%s</td></tr>", folder->entry->d_name, folder->entry->d_name, buffer);
-        folder = folder->sig;
-    }
-    freeNodo(folders);
+    else if(fieldOrder == NAME)
+        response_length += sprintf(response + response_length, "<html><head> <meta charset=\"UTF-8\"> <style> .bi-folder-fill-yellow{display:inline-block;width:1em;height:1em;vertical-align:-0.125em;background-image:url(\"data:image/svg+xml,%%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%%23f7d51d'%%3E%%3Cpath d='M14.5 6h-5.8l-1-2H4.5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM7 12H5v-2h2v2zm0-3H5V7h2v2z'/%%3E%%3C/svg%%3E\");background-repeat:no-repeat;background-size:1em} i { margin-right: 5px; } table{width:80%%;max-width:1000px;margin-left:auto;margin-right:auto;border-collapse:collapse}thead{background-color:#333;color:#fff}th{padding:12px}tbody tr:nth-child(even){background-color:#f2f2f2}td{padding:10px}td:first-child{text-align:left}td:last-child{text-align:center}td:nth-child(2){text-align:right;padding-right:20px} #sinh { user-select: none; text-decoration: none; color: black; } </style><title>Directorio %s</title></head><body><table><tr><th><a id=\"sinh\" href=\"?order=nameDesc\">Name</a></th><th><a id=\"sinh\" href=\"?order=sizeAsc\">Size</a></th><th><a id=\"sinh\" href=\"?order=dateAsc\">Last Modification Date</a></th></tr>", directory_path);
 
-    struct nodo* file = files;
-    while(file != NULL)
+    else if(fieldOrder == SIZE)
+        response_length += sprintf(response + response_length, "<html><head> <meta charset=\"UTF-8\"> <style> .bi-folder-fill-yellow{display:inline-block;width:1em;height:1em;vertical-align:-0.125em;background-image:url(\"data:image/svg+xml,%%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%%23f7d51d'%%3E%%3Cpath d='M14.5 6h-5.8l-1-2H4.5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM7 12H5v-2h2v2zm0-3H5V7h2v2z'/%%3E%%3C/svg%%3E\");background-repeat:no-repeat;background-size:1em} i { margin-right: 5px; } table{width:80%%;max-width:1000px;margin-left:auto;margin-right:auto;border-collapse:collapse}thead{background-color:#333;color:#fff}th{padding:12px}tbody tr:nth-child(even){background-color:#f2f2f2}td{padding:10px}td:first-child{text-align:left}td:last-child{text-align:center}td:nth-child(2){text-align:right;padding-right:20px} #sinh { user-select: none; text-decoration: none; color: black; } </style><title>Directorio %s</title></head><body><table><tr><th><a id=\"sinh\" href=\"?order=nameAsc\">Name</a></th><th><a id=\"sinh\" href=\"?order=sizeDesc\">Size</a></th><th><a id=\"sinh\" href=\"?order=dateAsc\">Last Modification Date</a></th></tr>", directory_path);
+
+    else if(fieldOrder == DATE)
+        response_length += sprintf(response + response_length, "<html><head> <meta charset=\"UTF-8\"> <style> .bi-folder-fill-yellow{display:inline-block;width:1em;height:1em;vertical-align:-0.125em;background-image:url(\"data:image/svg+xml,%%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' fill='%%23f7d51d'%%3E%%3Cpath d='M14.5 6h-5.8l-1-2H4.5c-1.1 0-2 .9-2 2v8c0 1.1.9 2 2 2h9c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zM7 12H5v-2h2v2zm0-3H5V7h2v2z'/%%3E%%3C/svg%%3E\");background-repeat:no-repeat;background-size:1em} i { margin-right: 5px; } table{width:80%%;max-width:1000px;margin-left:auto;margin-right:auto;border-collapse:collapse}thead{background-color:#333;color:#fff}th{padding:12px}tbody tr:nth-child(even){background-color:#f2f2f2}td{padding:10px}td:first-child{text-align:left}td:last-child{text-align:center}td:nth-child(2){text-align:right;padding-right:20px} #sinh { user-select: none; text-decoration: none; color: black; } </style><title>Directorio %s</title></head><body><table><tr><th><a id=\"sinh\" href=\"?order=nameAsc\">Name</a></th><th><a id=\"sinh\" href=\"?order=sizeAsc\">Size</a></th><th><a id=\"sinh\" href=\"?order=dateDesc\">Last Modification Date</a></th></tr>", directory_path);
+
+    if(fieldOrder != NAME)
     {
-        struct tm* t = localtime(&(file->st).st_mtime); 
-        char buffer [20];
-        strftime(buffer, 20, "%d-%m-%Y %H:%M", t);
-        char *size = formatearTamaño((file->st).st_size);
-        response_length += sprintf(response + response_length, "<tr><td><a href=\"%s/\" download=\"%s\">%s</a></td><td style=\"white-space: nowrap;\">%s</td><td>%s</td></tr>", file->entry->d_name, file->entry->d_name, file->entry->d_name, size, buffer);
-        file = file->sig;
-        free(size);
+        struct nodo* file = files;
+        while(file != NULL)
+        {
+            struct tm* t = localtime(&(file->st).st_mtime); 
+            char buffer [20];
+            strftime(buffer, 20, "%d-%m-%Y %H:%M", t);
+            char *size = formatearTamaño((file->st).st_size);
+            response_length += sprintf(response + response_length, "<tr><td><a href=\"%s/\" download=\"%s\">%s</a></td><td style=\"white-space: nowrap;\">%s</td><td>%s</td></tr>", file->entry->d_name, file->entry->d_name, file->entry->d_name, size, buffer);
+            file = file->sig;
+            free(size);
+        }
+        freeNodo(files);
+
+        struct nodo* folder = folders;
+        while(folder != NULL)
+        {
+            struct tm* t = localtime(&(folder->st).st_mtime);
+            char buffer [20];
+            strftime(buffer, 20, "%d-%m-%Y %H:%M", t); 
+            response_length += sprintf(response + response_length, "<tr><td><i class=\"bi bi-folder-fill-yellow\"></i><a href=\"%s/\">%s</a></td><td></td><td>%s</td></tr>", folder->entry->d_name, folder->entry->d_name, buffer);
+            folder = folder->sig;
+        }
+        freeNodo(folders);
     }
-    freeNodo(files);
+
+    else
+    {
+        struct nodo* folder = folders;
+        while(folder != NULL)
+        {
+            struct tm* t = localtime(&(folder->st).st_mtime);
+            char buffer [20];
+            strftime(buffer, 20, "%d-%m-%Y %H:%M", t); 
+            response_length += sprintf(response + response_length, "<tr><td><i class=\"bi bi-folder-fill-yellow\"></i><a href=\"%s/\">%s</a></td><td></td><td>%s</td></tr>", folder->entry->d_name, folder->entry->d_name, buffer);
+            folder = folder->sig;
+        }
+        freeNodo(folders);
+
+        struct nodo* file = files;
+        while(file != NULL)
+        {
+            struct tm* t = localtime(&(file->st).st_mtime); 
+            char buffer [20];
+            strftime(buffer, 20, "%d-%m-%Y %H:%M", t);
+            char *size = formatearTamaño((file->st).st_size);
+            response_length += sprintf(response + response_length, "<tr><td><a href=\"%s/\" download=\"%s\">%s</a></td><td style=\"white-space: nowrap;\">%s</td><td>%s</td></tr>", file->entry->d_name, file->entry->d_name, file->entry->d_name, size, buffer);
+            file = file->sig;
+            free(size);
+        }
+        freeNodo(files);
+    }
 
     response_length += sprintf(response + response_length, "</table></body></html>");
 
@@ -305,6 +420,48 @@ void handle_request(int client_socket, char* request) {
         return;
     }
     char* newpath = reemplazarPorcentajes(path, strlen(path));
+
+    int fieldOrder = NAME;
+    int Asc = TRUE;
+
+    char *again = strstr(newpath, "?");
+
+    if(again != NULL)
+    {
+        if(strcmp(again + 1, "order=nameAsc") == 0)
+        {
+            fieldOrder = NAME;
+            Asc = TRUE;
+        }
+        else if(strcmp(again + 1, "order=nameDesc") == 0)
+        {
+            fieldOrder = NAME;
+            Asc = FALSE;
+        }
+        else if(strcmp(again + 1, "order=sizeAsc") == 0)
+        {
+            fieldOrder = SIZE;
+            Asc = TRUE;
+        }
+        else if(strcmp(again + 1, "order=sizeDesc") == 0)
+        {
+            fieldOrder = SIZE;
+            Asc = FALSE;
+        }
+        else if(strcmp(again + 1, "order=dateAsc") == 0)
+        {
+            fieldOrder = DATE;
+            Asc = TRUE;
+        }
+        else if(strcmp(again + 1, "order=dateDesc") == 0)
+        {
+            fieldOrder = DATE;
+            Asc = FALSE;
+        }
+
+        again[0] = '\0';
+    }
+
     int len1 = strlen(init_path);
     int len2 = strlen(newpath);
     char newPath[len1 + len2];
@@ -313,7 +470,7 @@ void handle_request(int client_socket, char* request) {
     struct stat st;
     if (stat(newPath, &st) == 0) {
         if (S_ISDIR(st.st_mode)) {
-            send_directory_listing(client_socket, newPath);
+            send_directory_listing(client_socket, newPath, fieldOrder, Asc);
         }
         else {
             send_file(client_socket, newPath);
@@ -337,13 +494,20 @@ void* handle_client(void* arg) {
     return NULL;
 }
 
+void sigint_handler (int sig) {
+    close(server_socket);
+    printf("\n");
+    exit(EXIT_SUCCESS);
+}
+
 int main(int argc, char **argv) {
+    signal(SIGINT, sigint_handler);
     if(argc >= 3)
     {
         int PORT = atoi(argv[1]);
         init_path = argv[2];
 
-        int server_socket, client_socket;
+        int client_socket;
         struct sockaddr_in server_address, client_address;
         socklen_t address_length = sizeof(client_address);
         server_socket = socket(AF_INET, SOCK_STREAM, 0);
